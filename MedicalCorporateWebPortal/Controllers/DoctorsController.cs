050b18ab7f7fd71bcefab88b7eecde4def3f2dc5
imil4eg <emzilh@mail.ru>
2018-05-25 00:36:01 +0300

    Added record confirming page

--- MedicalCorporateWebPortal/Controllers/DoctorsController.cs
+++ MedicalCorporateWebPortal/Controllers/DoctorsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MedicalCorporateWebPortal.Models;
 
 namespace MedicalCorporateWebPortal.Controllers
 {
@@ -63,6 +64,13 @@ namespace MedicalCorporateWebPortal.Controllers
             var appointmentTime = _context.AppointmentTimes.Where(time => appointmentDate.Any(date => date.DateOfAppointmentID == time.DateOfAppointmentID));
             var providedServices = _context.ProvideServices.Where(service => service.DoctorID == doctor.ID);
             List<Service> services = null;
+            ServiceListViewModel mv = new ServiceListViewModel();
+            mv.Services = _context.Services.Where(service => providedServices.Any(ps => ps.ServiceID == service.ServiceID)).ToList();
+            mv.ServiceItems = new List<SelectListItem>();
+            for (int i = 0;i < mv.Services.Count; i++)
+            {
+                mv.ServiceItems.Add(new SelectListItem { Text = mv.Services[i].Name, Value = mv.Services[i].ServiceID.ToString() });
+            }
             if (providedServices.Any())
             {
                 services = _context.Services.Where(service => providedServices.Any(ps => ps.ServiceID == service.ServiceID)).ToList();
@@ -75,7 +83,7 @@ namespace MedicalCorporateWebPortal.Controllers
                     items.Add(new SelectListItem { Text = service.Name, Value = service.ServiceID.ToString() });
                 }
             }
-            var tuple = new Tuple<Doctor, IEnumerable<DateOfAppointment>, IEnumerable<AppointmentTime>, IEnumerable<Service>, User>(doctor, appointmentDate, appointmentTime, services, user);
+            var tuple = new Tuple<Doctor, IEnumerable<DateOfAppointment>, IEnumerable<AppointmentTime>, ServiceListViewModel, User>(doctor, appointmentDate, appointmentTime, mv, user);
             return View(tuple);
         }
 
@@ -84,6 +92,19 @@ namespace MedicalCorporateWebPortal.Controllers
             var services = _context.Services.Select(s => s);
 
             return View(services);
+        }
+
+        [HttpPost]
+        public IActionResult RecordConfirmation(int doctorId, DateTime date)
+        {
+            int serviceId = 0;
+            var doctor = _context.Doctors.SingleOrDefault(d => d.ID == doctorId);
+            var employee = _context.Employees.SingleOrDefault(e => e.EmployeeID == doctor.EmployeeID);
+            var user = _context.Users.SingleOrDefault(u => u.UserID == employee.UserID);
+            var service = _context.Services.SingleOrDefault(s => s.ServiceID == serviceId);
+            var appointmentDate = _context.AppointmentDates.SingleOrDefault(d => d.Date.Equals(date));
+
+            return View(new Tuple<Doctor, Service, DateOfAppointment, string, User>(doctor, service, appointmentDate, date.Date.TimeOfDay.ToString("HH:mm"), user));
         }
     }
 }
---
+++ MedicalCorporateWebPortal/Models/ServiceListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MedicalCorporateWebPortal.AppData;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MedicalCorporateWebPortal.Models
+{
+    public class ServiceListViewModel
+    {
+        public List<Service> Services;
+        public IList<SelectListItem> ServiceItems { get; set; }
+        public int SelectedServiceID { get; set; }
+    }
+}
